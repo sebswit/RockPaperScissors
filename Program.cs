102ad51ddfc5cf8727d47cfeffe0f4c23b1bdd65
sebswit <sebastian.switakowski@gmail.com>
2024-03-08 21:35:26 +0000

    added switch and if ststements

--- Program.cs
+++ Program.cs
@@ -10,18 +10,99 @@ namespace MyApp
             bool playAgain = true;
             String player;
             String computer;
+            String answer;
+
+
             while (playAgain)
             {
-                Console.Write("Enter ROCK, PAPER or SCISSORS: ");
-                player = Console.ReadLine();
-                player = player.ToUpper();
+                player = "";
+                computer = "";
+                answer = "";
 
-                Console.WriteLine(player);
+                while(player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.Write("Enter ROCK, PAPER or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                }
+                switch(random.Next(1, 4))
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
 
 
+                }
+
+                Console.WriteLine("Player: "+player);
+                Console.WriteLine("Computer: "+computer);
+
+                switch (player)
+                {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("Draw");
+                        }
+                        else if (computer == "SCISSORS")
+                        {
+                            Console.WriteLine("You win");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Loose");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "PAPER")
+                        {
+                            Console.WriteLine("Draw");
+                        }
+                        else if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You win");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Loose");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "SCISSORS")
+                        {
+                            Console.WriteLine("Draw");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You win");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Loose");
+                        }
+                        break;
+                }
+
+                Console.WriteLine("Play again (Y/N)");
+                answer = Console.ReadLine();
+                answer = answer.ToUpper();
+                 if (answer == "Y")
+                {
+                    playAgain = true;
+                }
+                 else
+                {
+                    playAgain = false;
+                }
             }
 
-
+            Console.WriteLine("Thanks for playing");
 
 
             Console.ReadKey();
