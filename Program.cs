b51f0ad18b41e1f210cd363ad8b1f0311591cad7
sebswit <sebastian.switakowski@gmail.com>
2024-03-08 21:03:41 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            while (playAgain)
+            {
+                Console.Write("Enter ROCK, PAPER or SCISSORS: ");
+                player = Console.ReadLine();
+                player = player.ToUpper();
+
+                Console.WriteLine(player);
+
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
